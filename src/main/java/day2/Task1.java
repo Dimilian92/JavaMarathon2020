d4425a89cc00a30d3c557832a2ad617774148cc8
dmitrisusskij <dvsussky@mts.ru>
2020-12-01 23:45:17 +0300

    Add day2 solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        String floors = s.nextLine();
+        int floorsInt = Integer.parseInt(floors);
+        if(floorsInt>=1 && floorsInt<=4){
+            System.out.println("Малоэтажный дом");
+        } else if (floorsInt>=5 && floorsInt<=8){
+            System.out.println("Среднеэтажный дом");
+        } else if (floorsInt>=9){
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+            Scanner s = new Scanner (System.in);
+            int a = s.nextInt();
+            int b = s.nextInt();
+            if(a>=b){
+                System.out.println("Некорректный ввод");
+            }
+            else {
+                int i = a;
+                while (i <=b){
+                    if(i%5==0 && i%10!=0){
+                        System.out.print(i+" ");
+                    }
+                    i++;
+                }
+            }
     }
 }
